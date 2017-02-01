de7e03d6729d831febe0a675c4af035f89b426c7
Robin de Mourat <robin.demourat@gmail.com>
2017-02-01 11:14:54 +0100

    fix typo

--- routes/oauth-proxy.js
+++ routes/oauth-proxy.js
@@ -2,7 +2,7 @@ const app = require('../server');
 
 const getToke = require('../services/githubOAuthProxy');
 
-app.post('/oauth-proxy/', (req, res) => {
+app.post('/oauth-proxy', (req, res) => {
   const code = req.body && req.body.code;
   console.log('asked with code', code);
   return getToken(code, res);
