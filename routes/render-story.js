e8a608eef8421c18e8a4d0ebebd751eb07994202
Robin de Mourat <robin.demourat@gmail.com>
2017-06-08 07:30:06 +0200

    test

--- routes/render-story.js
+++ routes/render-story.js
@@ -7,6 +7,7 @@ app.post('/render-story', (req, res) => {
     const story = req.body;
     // todo put a story format validation hook here
     if (true) {
+      console.log('render story', story);
       const html = renderStory(story);
       res.send(html);
     } else {
