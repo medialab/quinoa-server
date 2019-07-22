2e4b527a9588ab589e5dbc8c6ccc4c1e3c6db744
Robin de Mourat <robin.demourat@gmail.com>
2019-07-22 17:23:07 +0200

    update story player

--- src/helpers/storyAppBuilder/webpack.config.js
+++ src/helpers/storyAppBuilder/webpack.config.js
@@ -49,10 +49,6 @@ module.exports = {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader']
       },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
     ]
   }
 };
